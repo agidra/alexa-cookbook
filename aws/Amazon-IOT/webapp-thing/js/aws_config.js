5f2d716049c3f3f8195d2583b4272412ec25a373
Aditi <aditi.gidra@gmail.com>
2017-11-15 12:58:58 +0530

    Updated iot settings

--- aws/Amazon-IOT/webapp-thing/js/aws_config.js
+++ aws/Amazon-IOT/webapp-thing/js/aws_config.js
@@ -3,7 +3,7 @@
 // 1. Login to AWS IOT console, and select a Region at the top-right
 //      such as Ireland (eu-west-1)  or N. Virginia (us-east-1)
 
-const REGION         = 'us-east-1';  // eu-west-1
+const REGION         = 'us-west-1';  // eu-east-1
 
 // 2. Click on Registry, and add a new Thing, such as waterPump
 
@@ -12,7 +12,7 @@ const ThingName      = 'thing1';
 const SubscribeTopic = '$aws/things/' + ThingName + '/shadow/update/accepted';
 
 // 3. Click on the Interact menu item, to reveal the API Endpoint:
-const mqttEndpoint   = "a3npzlqqmmzqo.iot.us-east-1.amazonaws.com";
+const mqttEndpoint   = "a1eajdbrwzfng2.iot.eu-west-1.amazonaws.com";
 
 
 // 4. Login to the AWS Cognito console
@@ -30,4 +30,4 @@ const mqttEndpoint   = "a3npzlqqmmzqo.iot.us-east-1.amazonaws.com";
 //
 
 // Cognito Identity Pool ID
-const IdentityPoolId = 'us-east-1:583dd84a-7792-49a6-9ce5-5624f80378e7';
+const IdentityPoolId = 'eu-west-1:fb8a10f7-b813-4630-9f10-839343683fa5';
